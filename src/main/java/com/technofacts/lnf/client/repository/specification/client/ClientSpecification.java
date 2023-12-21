f3c1a6ac8aff10ab45331bef57d2d2b846dbca86
vamsi <vamsireddy.v@techno-facts.com>
2023-12-21 16:38:56 +0530

    search filter fix in client service

--- src/main/java/com/technofacts/lnf/client/repository/specification/client/ClientSpecification.java
+++ src/main/java/com/technofacts/lnf/client/repository/specification/client/ClientSpecification.java
@@ -1,5 +1,6 @@
 package com.technofacts.lnf.client.repository.specification.client;
 
+
 import com.technofacts.lnf.client.model.Client;
 import com.technofacts.lnf.util.SpecSearchCriteria;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -25,23 +26,37 @@ public class ClientSpecification implements Specification<Client> {
     public Predicate toPredicate(Root<Client> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder builder) {
         switch (criteria.getOperation()) {
             case EQUALITY:
-                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+                return builder.equal(
+                        builder.lower(root.get(criteria.getKey())),
+                        criteria.getValue().toString().trim().toLowerCase()
+                );
             case NEGATION:
-                return builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
+                return builder.notEqual(
+                        builder.lower(root.get(criteria.getKey())),
+                        criteria.getValue().toString().trim().toLowerCase()
+                );
             case GREATER_THAN:
-                return builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString());
+                return builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString().trim());
             case LESS_THAN:
-                return builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString());
-            case LIKE:
-                return builder.like(root.get(criteria.getKey()), criteria.getValue().toString());
+                return builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString().trim());
+            case LIKE, CONTAINS:
+                return builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        "%" + criteria.getValue().toString().trim().toLowerCase() + "%"
+                );
             case STARTS_WITH:
-                return builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+                return builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        criteria.getValue().toString().trim().toLowerCase() + "%"
+                );
             case ENDS_WITH:
-                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
-            case CONTAINS:
-                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
+                return builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        "%" + criteria.getValue().toString().trim().toLowerCase()
+                );
             default:
                 return null;
         }
     }
+
 }
