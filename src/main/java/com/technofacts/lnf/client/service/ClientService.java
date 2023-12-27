bd6ea688e050d17af968ea7cadb54b134b30745e
ajith <ajith@technofacts.co.uk>
2023-12-27 10:24:31 +0000

    HRMW-745 - Updated client search functionality with generic implementation

--- src/main/java/com/technofacts/lnf/client/service/ClientService.java
+++ src/main/java/com/technofacts/lnf/client/service/ClientService.java
@@ -4,12 +4,13 @@ import com.google.common.collect.Lists;
 import com.technofacts.lnf.client.converter.ClientConverter;
 import com.technofacts.lnf.client.model.Client;
 import com.technofacts.lnf.client.repository.ClientRepository;
-import com.technofacts.lnf.client.repository.specification.client.ClientSpecificationBuilder;
 import com.technofacts.lnf.dto.client.ClientDto;
 import com.technofacts.lnf.exception.LnFBadRequestException;
 import com.technofacts.lnf.exception.LnFEntityNotFoundException;
 import com.technofacts.lnf.exception.LnFException;
+import com.technofacts.lnf.service.specification.GenericSpecificationBuilder;
 import com.technofacts.lnf.util.RestUtil;
+import com.technofacts.lnf.util.specification.SpecificationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.data.domain.Page;
@@ -19,20 +20,16 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.function.Function;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Log
 public class ClientService {
-
-    private static final String SEARCH_REGEX_PATTERN = "([\\w]+)\\s*:\\s*([\\w.@\\- ]+?)(?=(,|$))";
 
     private final ClientRepository repository;
 
@@ -194,69 +191,23 @@ public class ClientService {
         List<Client> entities = repository.findAll(specification);
         return convertToDtos(entities);
     }
+
     private List<ClientDto> convertToDtos(List<Client> entities) {
         return entities.stream()
                 .map(ClientConverter::toTransportModel)
                 .filter(Objects::nonNull)
                 .toList();
     }
+
     private Specification<Client> buildClientSpecification(String search) {
-        final ClientSpecificationBuilder builder = new ClientSpecificationBuilder();
-        createSearchConditions(search, builder);
-        return builder.build();
+        GenericSpecificationBuilder<Client> clientBuilder = new GenericSpecificationBuilder<>();
+        Function<String, Class<?>> fieldClassForClient = this::getFieldClassFromClient;
+        return SpecificationUtil.buildSpecification(search, clientBuilder, fieldClassForClient);
     }
 
-    private void createSearchConditions(String search, ClientSpecificationBuilder builder) {
-        log.info(() -> String.format("search [%s]", search));
-        log.info(() -> String.format("builder [%s]", builder.toString()));
-        final Matcher matcher = Pattern.compile(SEARCH_REGEX_PATTERN, Pattern.CASE_INSENSITIVE).matcher(search);
-        while (matcher.find()) {
-            processSearchGroup(matcher, builder);
-        }
+    private Class<?> getFieldClassFromClient(String fieldName) {
+        return SpecificationUtil.getFieldClass(Client.class, fieldName);
     }
 
-    private void processSearchGroup(final Matcher matcher, final ClientSpecificationBuilder builder) {
-        String key = matcher.group(1).trim();
-        String value = matcher.group(2).trim();
-        log.info("Key: " + key + ", Value: " + value);
-        Class<?> fieldType = getFieldClass(key);
-        addCondition(builder, key, fieldType, value);
-    }
-
-    private void addCondition(final ClientSpecificationBuilder builder, final String key, Class<?> fieldType, final String value) {
-        if (fieldType != null) {
-            Object convertedValue = convertToFieldType(fieldType, value);
-            builder.with(key, ":", convertedValue);
-        }
-    }
-    private Class<?> getFieldClass(String fieldName) {
-        try {
-            Class<?> clazz = Class.forName("com.technofacts.lnf.client.model.Client");
-            Field field = clazz.getDeclaredField(fieldName);
-            return field.getType();
-        } catch (ClassNotFoundException | NoSuchFieldException e) {
-            return null;
-        }
-    }
-
-    private Object convertToFieldType(Class<?> fieldType, String value) {
-        if (fieldType.isEnum()) {
-            return getEnumConstant(fieldType, value);
-        } else if (fieldType == Integer.class || fieldType == int.class) {
-            return Integer.valueOf(value);
-        } else {
-            return value;
-        }
-    }
-
-    private Enum<?> getEnumConstant(Class<?> fieldType, String value) {
-        String uppercaseValue = value.toUpperCase();
-        for (Enum<?> enumConstant : ((Class<? extends Enum>) fieldType).getEnumConstants()) {
-            if (enumConstant.name().toUpperCase().equals(uppercaseValue)) {
-                return enumConstant;
-            }
-        }
-        return null;
-    }
 }
 
--- src/main/java/com/technofacts/lnf/client/service/ProjectService.java
+++ src/main/java/com/technofacts/lnf/client/service/ProjectService.java
@@ -1,25 +1,18 @@
 package com.technofacts.lnf.client.service;
 
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.google.common.collect.Lists;
-
 import com.technofacts.lnf.client.converter.ProjectConverter;
 import com.technofacts.lnf.client.model.Client;
 import com.technofacts.lnf.client.model.Project;
 import com.technofacts.lnf.client.repository.ClientRepository;
 import com.technofacts.lnf.client.repository.ProjectRepository;
-import com.technofacts.lnf.client.repository.specification.project.ProjectSpecificationBuilder;
 import com.technofacts.lnf.dto.client.ProjectDto;
 import com.technofacts.lnf.exception.LnFBadRequestException;
 import com.technofacts.lnf.exception.LnFEntityNotFoundException;
 import com.technofacts.lnf.exception.LnFException;
+import com.technofacts.lnf.service.specification.GenericSpecificationBuilder;
 import com.technofacts.lnf.util.RestUtil;
+import com.technofacts.lnf.util.specification.SpecificationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.data.domain.Page;
@@ -29,13 +22,16 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.function.Function;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Log
 public class ProjectService {
-
-    private static final String SEARCH_REGEX_PATTERN = "([\\w]+)\\s*:\\s*([\\w.@\\- ]+?)(?=(,|$))";
 
     private final ProjectRepository repository;
     private final ClientRepository clientRepository;
@@ -110,66 +106,18 @@ public class ProjectService {
                 .filter(Objects::nonNull)
                 .toList();
     }
+
     private Specification<Project> buildProjectSpecification(String search) {
-        final ProjectSpecificationBuilder builder = new ProjectSpecificationBuilder();
-        createSearchConditions(search, builder);
-        return builder.build();
+        GenericSpecificationBuilder<Project> clientBuilder = new GenericSpecificationBuilder<>();
+        Function<String, Class<?>> fieldClassForProject = this::getFieldClassFromProject;
+        return SpecificationUtil.buildSpecification(search, clientBuilder, fieldClassForProject);
     }
 
-    private void createSearchConditions(String search, ProjectSpecificationBuilder builder) {
-        log.info(() -> String.format("search [%s]", search));
-        log.info(() -> String.format("builder [%s]", builder.toString()));
-        final Matcher matcher = Pattern.compile(SEARCH_REGEX_PATTERN, Pattern.CASE_INSENSITIVE).matcher(search);
-        while (matcher.find()) {
-            processSearchGroup(matcher, builder);
-        }
+    private Class<?> getFieldClassFromProject(String fieldName) {
+        return SpecificationUtil.getFieldClass(Project.class, fieldName);
     }
 
-    private void processSearchGroup(final Matcher matcher, final ProjectSpecificationBuilder builder) {
-        String key = matcher.group(1).trim();
-        String value = matcher.group(2).trim();
-        log.info("Key: " + key + ", Value: " + value);
-        Class<?> fieldType = getFieldClass(key);
-        addCondition(builder, key, fieldType, value);
-    }
-
-    private void addCondition(final ProjectSpecificationBuilder builder, final String key, Class<?> fieldType, final String value) {
-        if (fieldType != null) {
-            Object convertedValue = convertToFieldType(fieldType, value);
-            builder.with(key, ":", convertedValue);
-        }
-    }
-    private Class<?> getFieldClass(String fieldName) {
-        try {
-            Class<?> clazz = Class.forName("com.technofacts.lnf.client.model.Project");
-            Field field = clazz.getDeclaredField(fieldName);
-            return field.getType();
-        } catch (ClassNotFoundException | NoSuchFieldException e) {
-            return null;
-        }
-    }
-
-    private Object convertToFieldType(Class<?> fieldType, String value) {
-        if (fieldType.isEnum()) {
-            return getEnumConstant(fieldType, value);
-        } else if (fieldType == Integer.class || fieldType == int.class) {
-            return Integer.valueOf(value);
-        } else {
-            return value;
-        }
-    }
-
-    private Enum<?> getEnumConstant(Class<?> fieldType, String value) {
-        String uppercaseValue = value.toUpperCase();
-        for (Enum<?> enumConstant : ((Class<? extends Enum>) fieldType).getEnumConstants()) {
-            if (enumConstant.name().toUpperCase().equals(uppercaseValue)) {
-                return enumConstant;
-            }
-        }
-        return null;
-    }
-
-    /**
+        /**
      * Returns projectDto from the projectId. Raises LnFEntityNotFoundException
      * if there is no project with the input projectId
      *
