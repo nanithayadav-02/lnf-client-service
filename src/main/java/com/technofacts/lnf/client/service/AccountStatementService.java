0fe36fa1d9fd32277b8c0577595a63221683bbc6
velankayala srinu <srinu.v@techno-facts.com>
2024-02-20 22:31:58 +0530

    statement of account

--- src/main/java/com/technofacts/lnf/client/service/AccountStatementService.java
+++ src/main/java/com/technofacts/lnf/client/service/AccountStatementService.java
@@ -4,7 +4,6 @@ import com.technofacts.lnf.dto.account.InvoiceDto;
 import com.technofacts.lnf.dto.account.InvoiceItemDto;
 import com.technofacts.lnf.dto.client.AccountStatementDto;
 import com.technofacts.lnf.dto.client.ProjectDto;
-import com.technofacts.lnf.exception.LnFEntityNotFoundException;
 import com.technofacts.lnf.service.account.InvoiceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -15,6 +14,7 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -25,19 +25,47 @@ public class AccountStatementService {
     private final InvoiceService invoiceService;
     private final ProjectService projectService;
 
-    public List<AccountStatementDto> findAccountStatement(LocalDate startDate, LocalDate endDate) {
+    public List<AccountStatementDto> findByClientId(UUID clientId) {
+        List<ProjectDto> projects = projectService.findProjectsByClientId(clientId);
+        List<InvoiceDto> invoices = projects.stream()
+                .map(ProjectDto::getId)
+                .flatMap(projectId -> retrieveInvoiceDetails(projectId).stream())
+                .toList();
+        return createAccountStatements(null, invoices);
+    }
+
+
+    public List<AccountStatementDto> findStatementForDesiredMonths(UUID clientId, int months) {
+        LocalDate startDate = LocalDate.now().minusMonths(months);
+        LocalDate endDate = LocalDate.now();
+        return findByClientIdAndDateRange(clientId, startDate, endDate);
+    }
+
+    public List<AccountStatementDto> findByClientIdAndDateRange(UUID clientId, LocalDate startDate, LocalDate endDate) {
         List<InvoiceDto> dtoList = retrieveInvoiceDetails(startDate, endDate);
-        return dtoList.stream()
-                .map(invoiceDto -> createAccountStatement(invoiceDto.getProjectId()))
+        return createAccountStatements(clientId, dtoList);
+    }
+
+    public List<AccountStatementDto> findByDateRange(LocalDate startDate, LocalDate endDate) {
+        List<InvoiceDto> dtoList = retrieveInvoiceDetails(startDate, endDate);
+        return createAccountStatements(null, dtoList);
+    }
+
+    private List<AccountStatementDto> createAccountStatements(UUID clientId, List<InvoiceDto> dtoList) {
+        Stream<InvoiceDto> filteredStream = dtoList.stream();
+        if (clientId != null) {
+            filteredStream = filteredStream.filter(invoiceDto -> invoiceDto.getClientId().equals(clientId));
+        }
+        return filteredStream
+                .map(this::createAccountStatement)
                 .toList();
     }
 
-    private AccountStatementDto createAccountStatement(UUID projectId) {
-
-        ProjectDto projectDto = retrieveProjectDetails(projectId);
-        InvoiceDto invoiceDto = retrieveInvoiceDetails(projectId);
+    private AccountStatementDto createAccountStatement(InvoiceDto invoiceDto) {
+        ProjectDto projectDto = retrieveProjectDetails(invoiceDto.getProjectId());
 
         AccountStatementDto statementDto = new AccountStatementDto();
+        statementDto.setInvoiceReference(invoiceDto.getReference());
         statementDto.setProjectId(projectDto.getId());
         statementDto.setProjectName(projectDto.getName());
         statementDto.setProjectType(projectDto.getType());
@@ -60,17 +88,23 @@ public class AccountStatementService {
         return projectService.findByProjectId(projectId);
     }
 
-    private InvoiceDto retrieveInvoiceDetails(UUID projectId) {
-        return invoiceService.search(projectId).stream().findFirst()
-                .orElseThrow(() -> new LnFEntityNotFoundException("Error occurred while retrieving invoice details: " + projectId));
+    private List<InvoiceDto> retrieveInvoiceDetails(UUID projectId) {
+        try {
+            return invoiceService.search(projectId);
+        } catch (Exception e) {
+            log.info("An error occurred while retrieving invoice details with projectId : " + projectId);
+            return Collections.emptyList();
+        }
     }
 
     private List<InvoiceDto> retrieveInvoiceDetails(LocalDate startDate, LocalDate endDate) {
         try {
             return invoiceService.findInvoicesByDateRange(startDate, endDate);
         } catch (Exception e) {
-            log.info("An error occurred while retrieving invoice details: " + e.getMessage());
+            log.info("An error occurred while retrieving invoice details : " + e.getMessage());
             return Collections.emptyList();
         }
     }
 }
+
+
