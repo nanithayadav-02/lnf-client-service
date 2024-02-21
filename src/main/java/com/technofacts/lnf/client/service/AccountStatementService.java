2ad3487a309bcac70cf61159684a89d44d6afd1f
velankayala srinu <srinu.v@techno-facts.com>
2024-02-21 15:03:35 +0530

    statement of account

--- src/main/java/com/technofacts/lnf/client/service/AccountStatementService.java
+++ src/main/java/com/technofacts/lnf/client/service/AccountStatementService.java
@@ -60,7 +60,10 @@ public class AccountStatementService {
     private List<AccountStatementDto> createAccountStatements(UUID clientId, List<InvoiceDto> dtoList) {
         Stream<InvoiceDto> filteredStream = dtoList.stream();
         if (clientId != null) {
-            filteredStream = filteredStream.filter(invoiceDto -> invoiceDto.getClientId().equals(clientId));
+            filteredStream = filteredStream.filter(invoiceDto -> {
+               UUID existingClientId = invoiceDto.getClientId();
+               return existingClientId != null && existingClientId.equals(clientId);
+            });
         }
         return filteredStream
                 .map(this::createAccountStatement)
