8c2d1e532d5dffb7e5b8c9b479630956d78ffd12
velankayala srinu <srinu.v@levernfulcrum.com>
2025-06-26 17:48:32 +0530

    test cases fixed

--- src/test/java/com/lnf/client/MockMvcTestConfig.java
+++ src/test/java/com/lnf/client/MockMvcTestConfig.java
@@ -27,7 +27,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @TestConfiguration
 public class MockMvcTestConfig {
-
     @Bean
     public MockMvc mockMvcWithTenant(WebApplicationContext context) {
         return MockMvcBuilders.webAppContextSetup(context)
