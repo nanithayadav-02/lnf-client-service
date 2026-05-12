e1204782c8fa01684aaf463ddfaf761a31d9630c
srinuvas <srinuvasarao.j@levernfulcrum.com>
2026-05-12 17:57:45 +0530

    updated the code

--- src/main/java/com/lnf/client/service/DataExportService.java
+++ src/main/java/com/lnf/client/service/DataExportService.java
@@ -384,7 +384,7 @@ public class DataExportService {
 
         return new Client(
                 code, name, pan, null, tan, status, workingFrom, agreementExpiryDate, serviceType, clientDetails, null,
-                null, null, null, addressSet, clientContactSet, escalationSet, gstSet, null,
+                null, null, null, null,addressSet, clientContactSet, escalationSet, gstSet, null,
                 null, null, null, null
         );
     }
